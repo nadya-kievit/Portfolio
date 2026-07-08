4d73c3159b3c7b64d23c8e027200c8af101fc066
Nadya Kievit <nadyanardos@gmail.com>
2026-07-08 10:30:41 -0700

    Added template from ryan balieiro

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/react-portfolio-template/',
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
+                        if (id.includes('swiper'))
+                            return 'swiper';
+                        return;
+                    }
+                }
+            }
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+            },
+        },
+    },
 })
